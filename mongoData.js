c676acb04fb994d17d5bc55ea333ceb962d83d4c
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 14:25:24 +0300

    Rokas

---
+++ mongoData.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const discordSchema = mongoose.Schema({
+    channelName: String,
+    conversation: [
+        {
+            message: String,
+            timestamp: String,
+            user: {
+                displayName: String,
+                email: String,
+                photo: String,
+                uid: String,
+            }
+        }
+    ]
+})
