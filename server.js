569fc738455ae1d8980b8feb3b57d61d6093a631
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 14:15:32 +0300

    Rokas

--- server.js
+++ server.js
@@ -9,13 +9,17 @@ const app = express();
 const port = process.env.PORT || 9003;
 // middlewares
 
+app.use(express.json());
+app.use(cors());
 
 // db config
 
-
+const mongoURI = 'mongodb+srv://admin:f9bdztDnqB1AojlZ@cluster0.mfuw3.mongodb.net/backend-discord?retryWrites=true&w=majority'
 // api routes
 app.get('/', (req, res) => res.status(200).send("Backend is working on 🚀"));
 
 // listen
 
 app.listen(port, () => console.log(`The app is listening on ${port}`));
+
+// f9bdztDnqB1AojlZ
